b5c7b97015f244da1b60dd1ae30be6ae2884f805
181847 <753090084@qq.com>
2018-02-28 18:14:24 +0800

    添加对vector2的基本测试。 尝试在写vector3之前，添加对vector3的单元测试。

--- ForCommonClasses/ForCommonClasses.cpp
+++ ForCommonClasses/ForCommonClasses.cpp
@@ -30,6 +30,70 @@ void AddTestUnit()
 	TEST_UNIT_END;
 #pragma endregion
 
+#pragma region test vector2 == and !=
+	TEST_UNIT_START("test vector2 == and !=")
+		CommonClass::vector2 cmp1(1.2f, 2.4f);
+		CommonClass::vector2 cmp2(1.2f + 1e-9f, 2.4f);
+
+		// the cmp2.m_x is a little bit bigger than cmp1.m_x,
+		// the operator should ignore the difference,
+		// and judge them as the equal vector.
+		errorLogger.LogIfNotEq(cmp1, cmp2);
+	TEST_UNIT_END;
+#pragma endregion
+
+#pragma region test vector2 +/ -/ *
+	TEST_UNIT_START("test vector2 +/ -/ *")
+		const Types::F32 comf1(1.2f), comf2(2.4f), comf3(3.5f), comf4(5.878f);
+		CommonClass::vector2 cmp1(comf1, comf2);
+		CommonClass::vector2 cmp2(comf3, comf4);
+
+		errorLogger.LogIfNotEq(
+			cmp1 + cmp2, 
+			CommonClass::vector2(comf1 + comf3, comf2 + comf4));
+
+		errorLogger.LogIfNotEq(
+			cmp1 - cmp2, 
+			CommonClass::vector2(comf1 - comf3, comf2 - comf4));
+
+		errorLogger.LogIfNotEq(
+			cmp1 * cmp2,
+			(comf1 * comf3 + comf2 * comf4) );
+	TEST_UNIT_END;
+#pragma endregion
+
+#pragma region test vector3 == and !=
+	TEST_UNIT_START("test vector3 == and !=")
+		CommonClass::vector3 cmp1(1.2f, 2.4f, 45.121f);
+		CommonClass::vector3 cmp2(1.2f + 1e-9f, 2.4f, 45.121f + 1e-10f);
+
+		// the cmp2.m_x is a little bit bigger than cmp1.m_x,
+		// the operator should ignore the difference,
+		// and judge them as the equal vector.
+		errorLogger.LogIfNotEq(cmp1, cmp2);
+	TEST_UNIT_END;
+#pragma endregion
+
+#pragma region test vector2 +/ -/ *
+	TEST_UNIT_START("test vector2 +/ -/ *")
+		const Types::F32 comf1(1.2f), comf2(2.4f), comf3(3.5f), comf4(5.878f), comf5(745.15), comf6(-45.4454);
+		CommonClass::vector3 cmp1(comf1, comf2, comf3);
+		CommonClass::vector3 cmp2(comf4, comf5, comf6);
+
+		errorLogger.LogIfNotEq(
+			cmp1 + cmp2,
+			CommonClass::vector3(comf1 + comf3, comf2 + comf4, comf));
+
+		errorLogger.LogIfNotEq(
+			cmp1 - cmp2,
+			CommonClass::vector2(comf1 - comf3, comf2 - comf4));
+
+		errorLogger.LogIfNotEq(
+			cmp1 * cmp2,
+			(comf1 * comf3 + comf2 * comf4));
+	TEST_UNIT_END;
+#pragma endregion
+
 }// AddTestUnit()
 
 }// namespace TestUnit
--- ForCommonClasses/ReadMe.md
+++ ForCommonClasses/ReadMe.md
@@ -1,3 +1,6 @@
 ﻿# ForCommonClasses项目概述
 
 此项目用于测试**项目CommonClasses**中编写的代码。
+
+## 测试涉及的类
+* vector2
