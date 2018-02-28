2ac3337a48830c05a2a7f61729500ae7ebff9d3a
181847 <753090084@qq.com>
2018-02-28 15:45:34 +0800

    实现vector2的主要部分以及计算函数。

--- CommonClasses/vector2.cpp
+++ CommonClasses/vector2.cpp
@@ -1,8 +1,14 @@
 #include "vector2.h"
 
-
+namespace CommonClass
+{
 
 vector2::vector2()
+{
+}
+
+vector2::vector2(const Types::F32 & x, const Types::F32 & y)
+	:m_x(x), m_y(y)
 {
 }
 
@@ -10,3 +16,25 @@ vector2::vector2()
 vector2::~vector2()
 {
 }
+
+vector2 operator+(const vector2 & a, const vector2 & b)
+{
+	return vector2(a.m_x + b.m_x, a.m_y + b.m_y);
+}
+
+vector2 operator-(const vector2 & a, const vector2 & b)
+{
+	return vector2(a.m_x - b.m_x, a.m_y - b.m_y);
+}
+
+vector2 operator*(const vector2 & a, const vector2 & b)
+{
+	return vector2(a.m_x * b.m_x, a.m_y * b.m_y);
+}
+
+vector2 dotProd(const vector2 & a, const vector2 & b)
+{
+	return vector2(a.m_x * b.m_x, a.m_y * b.m_y);
+}
+
+}// namespace CommonClass
--- CommonClasses/vector2.h
+++ CommonClasses/vector2.h
@@ -1,11 +1,33 @@
 #pragma once
+#include "CommonTypes.h"
 
+namespace CommonClass
+{
+
+// vector2 have two float component.
 class vector2
 {
+
+public:
+	union
+	{
+		struct 
+		{
+			Types::F32 m_x, m_y;
+		};
+		Types::F32 m_arr[2];
+	};
+
 public:
 	vector2();
+	vector2(const Types::F32 & x, const Types::F32 & y);
 	~vector2();
-
 
 };
 
+vector2 operator + (const vector2 & a, const vector2 & b);
+vector2 operator - (const vector2 & a, const vector2 & b);
+vector2 operator * (const vector2 & a, const vector2 & b);
+vector2 dotProd    (const vector2 & a, const vector2 & b);	// same as operator *
+
+}// namespace CommonClass
