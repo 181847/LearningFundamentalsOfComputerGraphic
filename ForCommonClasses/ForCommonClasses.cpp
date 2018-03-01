1c8f410cd99f0b89884f4975d9054f5b2424e628
181847 <753090084@qq.com>
2018-03-01 19:05:00 +0800

    设计对“RGBA8”类的测试。

--- ForCommonClasses/ForCommonClasses.cpp
+++ ForCommonClasses/ForCommonClasses.cpp
@@ -118,6 +118,146 @@ void AddTestUnit()
 	TEST_UNIT_END;
 #pragma endregion
 
+#pragma region test RGBA8 +/ -/ */ *(scalar)/ /(scalar)
+	TEST_UNIT_START("test RGBA8 +/ -/ */ *(scalar)/ /(scalar)")
+		RandomTool::MTRandom mtr;
+		const unsigned int MAX_RAND_INT = 255;
+
+		/*!
+			\brief clamp the channel of pixel <= 255
+		*/
+		auto clampChannel = [](Types::U32 ch)-> Types::U8
+		{
+			if (ch >= 0xff)
+			{
+				return 0xff;
+			}
+			else
+			{
+				return ch;
+			}
+		};
+
+		/*! only for test
+			\brief revert the channel bigger than 255 wrap to 0
+			only for the RGB substraction when the second operator is bigger,
+			make the result up to the max of U32.
+		*/
+		auto revertChannel = [](Types::U32 ch)->Types::U8
+		{
+			if (ch > 0xff)
+			{
+				return 0;
+			}
+			else
+			{
+				return ch;
+			}
+		};
+		
+		for (int i = 0; i < 200; ++i)
+		{
+			/*!
+				make up the RGBA channel
+			*/
+			const Types::U32	comu1(mtr.Random(MAX_RAND_INT)),
+								comu2(mtr.Random(MAX_RAND_INT)),
+								comu3(mtr.Random(MAX_RAND_INT)),
+								comu4(mtr.Random(MAX_RAND_INT)),
+								comu5(mtr.Random(MAX_RAND_INT)),
+								comu6(mtr.Random(MAX_RAND_INT)),
+								comu7(mtr.Random(MAX_RAND_INT)),
+								comu8(mtr.Random(MAX_RAND_INT));
+
+			/*!
+				scale the RGB with float.
+			*/
+			const Types::F32	comf1(mtr.Random()),
+								comf2(mtr.Random());
+
+			CommonClass::RGBA8 cmp1(comu1, comu2, comu3, comu4);
+			CommonClass::RGBA8 cmp2(comu5, comu6, comu7, comu8);
+
+			/*!
+				\brief recover the cmp1's value
+			*/
+			auto recoverCMP1 = [&cmp1, comu1, comu2, comu3, comu4]()
+			{
+				cmp1 = CommonClass::RGBA8(comu1, comu2, comu3, comu4);
+			};
+
+			// cmp1 + cmp2 rgb only
+			cmp1.AddRGB(cmp2);
+			errorLogger.LogIfNotEq( cmp1,
+				CommonClass::RGBA8(clampChannel(comu1 + comu5), clampChannel(comu2 + comu6), clampChannel(comu3 + comu7), comu4));
+			recoverCMP1();
+
+			// cmp1 - cmp2 rgb only
+			errorLogger.LogIfNotEq(
+				cmp1.SubRGB(cmp2),
+				CommonClass::RGBA8(revertChannel(comu1 - comu5), revertChannel(comu2 - comu6), revertChannel(comu3 - comu7), comu4));
+			recoverCMP1();
+			
+			// cmp1 * cmp2 rgb only
+			errorLogger.LogIfNotEq(
+				cmp1.MulRGB(cmp2),
+				CommonClass::RGBA8(clampChannel((comu1 * comu5) >> 8), clampChannel((comu2 - comu6) >> 8), clampChannel((comu3 - comu7) >> 8), comu4));	// '>> 8' because we scale 8bit channel value back to [0, 255]
+			recoverCMP1();
+
+			// cmp1 * scalar rgb only
+			errorLogger.LogIfNotEq(
+				cmp1.MulRGB(comf1),
+				CommonClass::RGBA8(clampChannel(comu1 * comf1), clampChannel(comu2 * comf1), clampChannel(comu3 * comf1), comu4));
+			recoverCMP1();
+
+			// cmp1 / scalar rgb only
+			errorLogger.LogIfNotEq(
+				cmp1.DivRGB(comf2),
+				CommonClass::RGBA8(clampChannel(comu1 / comf2), clampChannel(comu2 / comf2), clampChannel(comu3 / comf2), comu4));
+			recoverCMP1();
+
+			// alpha add
+			cmp1.AddAlpha(comu8);
+			errorLogger.LogIfNotEq(
+				cmp1.m_a,
+				clampChannel(comu4 + comu8)
+			);
+			recoverCMP1();
+
+			// alpha sub
+			cmp1.SubAlpha(comu8);
+			errorLogger.LogIfNotEq(
+				cmp1.m_a,
+				revertChannel(comu4 - comu8)
+			);
+			recoverCMP1();
+
+			// alpha mul
+			cmp1.MulAlpha(comu8);
+			errorLogger.LogIfNotEq(
+				cmp1.m_a,
+				clampChannel((comu1 * comu8) >> 8)
+			);
+
+			// alpha mul float
+			cmp1.MulAlpha(comf1);
+			errorLogger.LogIfNotEq(
+				cmp1.m_a,
+				clampChannel((comu1 * comf1))
+			);
+
+			// alpha div float
+			cmp1.DivAlpha(comf2);
+			errorLogger.LogIfNotEq(
+				cmp1.m_a,
+				clampChannel((comu1 / comf2))
+			);
+
+
+		}
+	TEST_UNIT_END;
+#pragma endregion
+
 }// AddTestUnit()
 
 }// namespace TestUnit
