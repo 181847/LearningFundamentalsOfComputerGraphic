4a38adb1f0acb30f24788d7aea4b7707aab629a5
181847 <753090084@qq.com>
2018-03-02 19:17:41 +0800

    添加RGBA类的声明部分

---
+++ CommonClasses/RGBA.h
@@ -0,0 +1,96 @@
+#pragma once
+#include "CommonTypes.h"
+
+namespace CommonClass
+{
+
+class RGBA
+{
+public:
+	union
+	{
+		struct
+		{
+			Types::F32 m_r, m_g, m_b, m_a;
+		};
+		Types::F32 m_arr[4];
+	};
+
+	/*!
+		\brief some useful constant value
+	*/
+	const static Types::F32 MIN_CHANNEL_VALUE;
+	const static Types::F32 MAX_CHANNEL_VALUE;
+	const static Types::F32 HALF_CHANNEL_VALUE;
+	const static Types::F32 ALPHA_CHANNEL_OPAQUE;
+	const static Types::F32 ALPHA_CHANNEL_TRANSPARENT;
+
+public:
+	RGBA();
+	RGBA(const Types::F32 & r, const Types::F32 & g, const Types::F32 & b, const Types::F32 & a = ALPHA_CHANNEL_OPAQUE);
+	~RGBA();
+
+	/*!
+		\brief let the rgb channel equal to the source
+		\param source the pixel hold desired rgb value
+		it will keep the original alpha value.
+	*/
+	void AssignRGB(const RGBA & source);
+
+	/*!
+		\brief increase rgb value by the source
+		\param source the pixel hold rgb value to add
+		if overflow, clamp to max value,
+		ignore alpha channel.
+	*/
+	void AddRGB(const RGBA & source);
+	
+	/*!
+		\brief decrease rgb value by the source
+		\param source the pixel hold rgb value to add
+		if negative overflow, clamp to min value,
+		ignore alpha channel.
+	*/
+	void SubRGB(const RGBA & source);
+
+	/*!
+		\brief scale the this pixel's rgb value by another pixel's correspond rgb value
+		\param scalePixel how much to scale the rgb
+		alpha will be ignored.
+	*/
+	void MulRGB(const RGBA & scalePixel);
+
+	/*!
+		\brief multiply by a float.
+		\param scalar scale value, a negative value will cause exception.
+	*/
+	void MulRGB(const Types::F32 & scalar);
+
+	/*!
+		\brief divided by a float
+		\param scalar divided value, a negative value will cause exception.
+	*/
+	void DivRGB(const Types::F32 & scalar);
+
+	/*!
+		\brief add to the alpha, clamp it between [0.0f, 1.0f]
+	*/
+	void AddAlpha(const Types::F32 & alpha);
+	
+	/*!
+		\brief substract the alpha, clamp it between [0.0f, 1.0f]
+	*/
+	void SubAlpha(const Types::F32 & alpha);
+	
+	/*!
+		\brief multiply by the alpha, clamp it between [0.0f, 1.0f]
+	*/
+	void MulAlpha(const Types::F32 & scale);
+	
+	/*!
+		\brief divided by the alpha, clamp it between [0.0f, 1.0f]
+	*/
+	void DivAlpha(const Types::F32 & scale);
+};
+
+}// namespace CommonClass
