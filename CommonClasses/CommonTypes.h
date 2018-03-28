80a2d1426baa7b3c4ceeeddfe812851a0a47e6fc
181847 <753090084@qq.com>
2018-03-28 22:41:42 +0800

    在CommonTypes.h中添加一些常用的常量。

--- CommonClasses/CommonTypes.h
+++ CommonClasses/CommonTypes.h
@@ -8,4 +8,14 @@ using I32 = int;
 using U32 = unsigned int;
 using U8  = unsigned char;
 
+namespace Constant
+{
+	
+const Types::F32 EPSILON_F32 = 1e-20f;
+const Types::F32 MIN_F32 = std::numeric_limits<Types::F32>::min();
+const Types::F32 MAX_F32 = std::numeric_limits<Types::F32>::max();
+
+
+}
+
 }// namespace Types
