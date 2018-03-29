91eaeddd5161a2cb2d2f3cb3611e49f1fb1e70b7
181847 <753090084@qq.com>
2018-03-29 18:49:31 +0800

    添加透视摄像机。

---
+++ CommonClasses/PerspectiveCamera.cpp
@@ -0,0 +1,25 @@
+#include "PerspectiveCamera.h"
+
+namespace CommonClass
+{
+
+PerspectiveCamera::PerspectiveCamera(const Types::F32 focalLength, CommonClass::vector3 & origin, const CommonClass::vector3 & lookAt, const CommonClass::vector3 & lookUp)
+	:Camera(origin, lookAt, lookUp), m_focalLenth(focalLength)
+{
+}
+
+PerspectiveCamera::~PerspectiveCamera()
+{
+}
+
+Ray PerspectiveCamera::GetRay(const Types::U32 x, const Types::U32 y)
+{
+	Types::F32 outU, outV;
+	m_film->GetPixelUV(x, y, outU, outV);
+
+	vector3 direction(-m_focalLenth * m_w + outU * m_u + outV * m_v);
+
+	return Ray(m_origin, direction);
+}
+
+} // namespace CommonClass
