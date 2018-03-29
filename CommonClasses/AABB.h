45429543f4100bec5c18499f78edf5217bdaedb9
181847 <753090084@qq.com>
2018-03-29 14:39:14 +0800

    增加注释

--- CommonClasses/AABB.h
+++ CommonClasses/AABB.h
@@ -30,8 +30,9 @@ public:
 	/*!
 		\brief whether the ray hit the box with interval [t0, t1].
 		\param ray ray to test
-		\param t0 min intersect dist(typical to be 0.0f)
+		\param t0 min intersect dist(typical to be >= 0.0f. NOT ensure the negative number will work)
 		\param t1 max intersect dist
+		\param pHitRec return hit record, but the AABB is a basic collision shape, the record may only have the t(distance to ray origin), other property like diffuse/normal may not be recorded.
 		In the distance interval, if ray dosen't hit the box,
 		it should never hit the inside object.
 	*/
