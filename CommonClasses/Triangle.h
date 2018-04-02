b175cf059a2d989c95f02c1fda2cf38b24da5db2
181847 <753090084@qq.com>
2018-04-02 21:11:57 +0800

    实现简单的三角形类。

---
+++ CommonClasses/Triangle.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "vector3.h"
+#include "Surface.h"
+#include <array>
+
+namespace CommonClass
+{
+
+
+class Triangle
+	:public Surface
+{
+public:
+	std::array<vector3, 3> m_points;
+public:
+	Triangle(const vector3& p0, const vector3& p1, const vector3& p2);
+	Triangle(std::array<vector3, 3>& threePoints);
+	~Triangle();
+
+	virtual bool Hit(const Ray& ray, const Types::F32 t0, const Types::F32 t1, HitRecord* pHitRec) override;
+	virtual AABB BoundingBox() override;
+
+
+};
+/*! ensurance, extra 4 bytes stands for the virtual pointer int the Surface*/
+static_assert(sizeof(Triangle) == 4 + 3 * sizeof(vector3), "size of Triangle is wrong");
+
+} // namespace CommonClass
