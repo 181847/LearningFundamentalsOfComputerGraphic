67fb2c1cf3e6649ab96612d83d979dd0d616c0eb
181847 <753090084@qq.com>
2018-04-03 20:31:58 +0800

    增加Scene类用于保存所有可渲染的物体。

---
+++ CommonClasses/Scene.cpp
@@ -0,0 +1,47 @@
+#include "Scene.h"
+
+namespace CommonClass
+{
+
+Scene::Scene()
+{
+	// empty
+}
+
+Scene::~Scene()
+{
+	// empty
+}
+
+void Scene::Add(std::unique_ptr<Surface> surf)
+{
+	m_surfaces.push_back(std::move(surf));
+}
+
+bool Scene::Hit(const Ray & ray, const Types::F32 t0, const Types::F32 t1, HitRecord * pHitRec)
+{
+	Types::F32 t = t1;
+	bool isHit = false;
+
+	for (auto & surf : m_surfaces)
+	{
+		// when hit with bounding box, the t is limited to [0.0f, t1], 
+		// because it's possible that "RayOrigin(0.0f) ----> BoundingBox ------> t0 -------> Object -------> t1"
+		// which the ray (dist belong to [t0, t1]) doesn't hit the BoundingBox but hit the Object, so set t0 to 0.0f to avoid this mistake.
+		// However we do not modify the t1, because if a ray cannot hit a BoundingBox, it cannot hit the Object.
+		if (surf->BoundingBox().Hit(ray, 0.0f, t, pHitRec)) 
+		{
+			if (surf->Hit(ray, t0, t, pHitRec))
+			{
+				isHit = true;
+				if (pHitRec->m_hitT < t)
+				{
+					t = pHitRec->m_hitT;
+				}
+			}
+		}
+	}
+	return isHit;
+}
+
+} // namespace CommonClass
---
+++ CommonClasses/Scene.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <vector>
+#include <memory>
+#include "Ray.h"
+#include "HitRecord.h"
+#include "Surface.h"
+
+namespace CommonClass
+{
+
+/*!
+	\brief Scene arrange all the lights and shading object, compute the closest hit point of a ray.
+*/
+class Scene
+{
+private:
+	std::vector<std::unique_ptr<Surface>> m_surfaces;
+
+public:
+	Scene();
+	Scene(const Scene&) = delete;
+	Scene& operator = (const Scene&) = delete;
+	~Scene();
+
+public:
+	/*!
+		\brief add a new surface object to the scene, such as a sphere or a triangle.
+		\param surf the new surface
+	*/
+	void Add(std::unique_ptr<Surface> surf);
+
+	/*!
+		\brief find the closest hit point in the scene.
+		\param ray the ray to cast
+		\param t0 minimum distance(include t0, this should be positive, no gaurantee about that the negative value will work properly.).
+		\param t1 maxmum distance(include t1)
+	*/
+	bool Hit(const Ray& ray, const Types::F32 t0, const Types::F32 t1, HitRecord* pHitRec);
+};
+
+} // namespace CommonClass
