dd91f0cda1c3b19fac5df4750ea00871ffa62b0f
181847 <753090084@qq.com>
2018-04-06 23:18:17 +0800

    更新Camera中获取光线的测试用例。

--- ForCommonClasses/ForCommonClasses.cpp
+++ ForCommonClasses/ForCommonClasses.cpp
@@ -469,8 +469,8 @@ TEST_MODULE_START
 		Film tfilm(WIDTH, HEIGHT, -1.0f, 1.0f, -1.0f, 1.0f);
 
 		RGBA pixelSetter;
-        TRGBA pixelSetterT;
 		pixelSetter.SetChannel<RGBA::B>(0.5f);
+        TRGBA pixelSetterT;
         pixelSetterT.SetChannel<TRGBA::B>(0.5f);
 
 		for (int x = 0; x < WIDTH; ++x)
@@ -598,16 +598,22 @@ TEST_MODULE_START
 
 		RGBA pixelSetter;
 		pixelSetter.SetChannel<RGBA::B>(0.5f);
+        TRGBA pixelSetterT;
+        pixelSetterT.SetChannel<TRGBA::B>(0.5f);
 
 		for (int x = 0; x < WIDTH; ++x)
 		{
 			for (int y = 0; y < HEIGHT; ++y)
 			{
-				pixelSetter.SetChannel<CommonClass::RGBA::R>(x / 512.0f);
+				/*pixelSetter.SetChannel<CommonClass::RGBA::R>(x / 512.0f);
 				pixelSetter.SetChannel<CommonClass::RGBA::G>(y / 512.0f);
-				pixelSetter.SetChannel<CommonClass::RGBA::A>((x + y) / 1024.0f);
+				pixelSetter.SetChannel<CommonClass::RGBA::A>((x + y) / 1024.0f);*/
 
-				orthoCamera.IncomeLight(x, y, pixelSetter);
+
+                pixelSetterT.SetChannel<TRGBA::R>(x / 512.0f);
+                pixelSetterT.SetChannel<TRGBA::G>(y / 512.0f);
+                pixelSetterT.SetChannel<TRGBA::A>((x + y) / 1024.0f);
+				orthoCamera.IncomeLight(x, y, pixelSetterT);
 			}
 		}
 
