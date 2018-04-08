e0471c817e8ed55606a2a7a1de358163c463356b
181847 <753090084@qq.com>
2018-04-08 20:22:35 +0800

    在Material类当中添加光滑程度，以及设置一个辅助函数用于通过折射率设置Fresnel参数

--- CommonClasses/Material.cpp
+++ CommonClasses/Material.cpp
@@ -17,13 +17,25 @@ Material::Material(const RGB & kDiffuse, const RGB & rFresnel_0)
 Material::Material(const RGB & kDiffuse, const unsigned int & reflectIndex)
     :m_kDiffuse(kDiffuse)
 {
-    Types::F32 fresenel0 = ( 1.0f * (reflectIndex - 1) ) / (reflectIndex + 1);
-    fresenel0 *= fresenel0; // square it
-    m_rFresnel_0 = RGB(fresenel0, fresenel0, fresenel0);
+    SetRFresnel0(reflectIndex);
 }
 
 Material::~Material()
 {
+}
+
+void Material::SetRFresnel0(const unsigned int & reflectIndex)
+{
+    Types::F32 fresenel0 = (1.0f * (reflectIndex - 1)) / (reflectIndex + 1);
+    fresenel0 *= fresenel0; // square it
+    m_rFresnel_0 = RGB(fresenel0, fresenel0, fresenel0);
+}
+
+RGB Material::RFresnel(const Types::F32 & cosTheta)
+{
+    Types::F32 cosThetaPow5 = (1 - cosTheta);
+    cosThetaPow5 = cosThetaPow5 * cosThetaPow5 * cosThetaPow5 * cosThetaPow5 * cosThetaPow5;
+    return m_rFresnel_0 + (RGB::WHITE - m_rFresnel_0) * cosThetaPow5;
 }
 
 } // namespace CommonClass
--- CommonClasses/Material.h
+++ CommonClasses/Material.h
@@ -20,11 +20,32 @@ public:
     */
     RGB     m_rFresnel_0;
 
+    Types::F32 m_shinness = 2;
+
 public:
     Material();
     Material(const RGB& kDiffuse, const RGB& rFresnel_0);
     Material(const RGB& kDiffuse, const unsigned int& reflectIndex);
     ~Material();
+
+    /*!
+        \brief set reflect fresnel coefficient by reflect index.
+        \param reflectIndex the reflect index respect to air (whose reflect index is one).
+    */
+    void SetRFresnel0(const unsigned int& reflectIndex);
+
+    /*!
+        \brief get reflect Fresnel coefficient respect to cosTheta,
+        \param cosThera cosin of the angle of normal and view ray.
+          normal
+        \   A   /eye
+         \  |  /
+          \ |_/
+           \|/
+        -------------
+        
+    */
+    RGB RFresnel(const Types::F32& cosTheta);
 };
 
 } // namespace CommonClass
