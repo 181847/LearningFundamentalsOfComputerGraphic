ead4d3ed0b1c7b352e604161e211d0ac1f898ff2
181847 <753090084@qq.com>
2018-04-08 13:01:24 +0800

    在HitRecord中添加碰撞表面的漫反射属性。

--- CommonClasses/HitRecord.h
+++ CommonClasses/HitRecord.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "vector3.h"
+#include "ColorTemplate.h"
 
 namespace CommonClass
 {
@@ -7,14 +8,30 @@ namespace CommonClass
 class HitRecord
 {
 public:
+    /*!
+        \brief hit distance from ray origin.
+    */
 	Types::F32	m_hitT;
+
+    /*!
+        \brief the hit point location.
+    */
 	vector3		m_hitPoint;
+
+    /*!
+        \brief the normal of the hit point.
+    */
 	vector3		m_normal;
 
 	/*!
-		\brief is the hit position is the back of the surface?
+		\brief WARNING!! This property is not correct for all hit object, just an experimental property of only triangle hit test, is the hit position is the back of the surface?
 	*/
 	bool        m_isBackFace = false;
+
+    /*!
+        \brief the diffuse coefficient of the hit point.
+    */
+    RGB         m_kDiffuse;
 
 public:
 };
