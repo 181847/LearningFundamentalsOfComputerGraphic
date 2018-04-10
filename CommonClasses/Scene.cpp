135ff0ecdb0b0443ce3f35671f23878c91093e1d
181847 <753090084@qq.com>
2018-04-10 13:42:49 +0800

    修正Scene当中包围盒信息覆盖正确碰撞信息的Bug，优化部分代码的执行。

--- CommonClasses/Scene.cpp
+++ CommonClasses/Scene.cpp
@@ -24,13 +24,17 @@ bool Scene::Hit(const Ray & ray, const Types::F32 t0, const Types::F32 t1, HitRe
 	Types::F32 t = t1;
 	bool isHit = false;
 
+    // MUST use another HitRecord for bounding box ray hit test.
+    // if not the information will influence the normal surface hit test.
+    HitRecord recForBBox;
 	for (auto & surf : m_surfaces)
 	{
 		// when hit with bounding box, the t is limited to [0.0f, t1], 
 		// because it's possible that "RayOrigin(0.0f) ----> BoundingBox ------> t0 -------> Object -------> t1"
 		// which the ray (dist belong to [t0, t1]) doesn't hit the BoundingBox but hit the Object, so set t0 to 0.0f to avoid this mistake.
 		// However we do not modify the t1, because if a ray cannot hit a BoundingBox, it cannot hit the Object.
-		if (surf->BoundingBox().Hit(ray, 0.0f, t, pHitRec)) 
+        if (surf->BoundingBox().Hit(ray, 0.0f, t, &recForBBox))
+        //if (surf->BoundingBox().Hit(ray, 0.0f, t, pHitRec))
 		{
 			if (surf->Hit(ray, t0, t, pHitRec))
 			{
@@ -40,15 +44,12 @@ bool Scene::Hit(const Ray & ray, const Types::F32 t0, const Types::F32 t1, HitRe
 					t = pHitRec->m_hitT;
 				}
 			}
-		}
+        }
 	}
 
     if (isHit)
     {
-        pHitRec->m_hitPoint = ray.m_origin + pHitRec->m_hitT * ray.m_direction;
-
-        // push the hit point out from the surface a little.
-        pHitRec->m_hitPoint = pHitRec->m_hitPoint + Surface::s_offsetHitT * pHitRec->m_normal;
+        pHitRec->m_hitPoint = ray.m_origin + (pHitRec->m_hitT + Surface::s_offsetHitT) * ray.m_direction;
     }
 
 	return isHit;
@@ -61,20 +62,24 @@ RGB Scene::RayColor(const Ray & ray, const Types::F32 t0, const Types::F32 t1, u
     {
         RGB color;
 
+        // ambient light
         color = hitRec.m_material.m_kDiffuse * this->m_ambient;
 
-        vector3 toLight = m_pointLight.ToMeFrom(hitRec.m_hitPoint);
+        // reflection
+        if (reflectLayerIndex > 0)
+        {
+            vector3 reflectVec = Reflect(ray.m_direction, hitRec.m_normal);
+            color = color + hitRec.m_material.m_shinness / 32.0f * RayColor(Ray(hitRec.m_hitPoint, reflectVec), 0.0f, 1000.0f, reflectLayerIndex - 1);
+        }
+
+        // shadow test
+        Types::F32 toLightDist = 0.0f;
+        vector3 toLight = m_pointLight.ToMeFrom(hitRec.m_hitPoint, &toLightDist);
 
         Ray shadowRayTest = Ray(hitRec.m_hitPoint, toLight);
 
-        if (reflectLayerIndex > 0)
-        {
-            vector3 reflectVec = ray.m_direction - 2 * (dotProd(ray.m_direction, hitRec.m_normal)) * hitRec.m_normal;
-            color = color + hitRec.m_material.m_shinness / 32.0f * RayColor(Ray(hitRec.m_hitPoint, reflectVec), t0, t1, reflectLayerIndex - 1);
-        }
-
         // is the hit point in the shadow?
-        if (!this->Hit(shadowRayTest, 0.0f, 1000.0f, &shadowHitRec))
+        if (!this->Hit(shadowRayTest, 0.0f, toLightDist, &shadowHitRec))
         {// yes it's in the shadow.
             vector3 toEye = -ray.m_direction;
             vector3 halfVec = Normalize(toEye + toLight);
@@ -91,6 +96,9 @@ RGB Scene::RayColor(const Ray & ray, const Types::F32 t0, const Types::F32 t1, u
                 + lightStrength 
                 * (hitRec.m_material.m_kDiffuse 
                     + fresnelCoefficient * shinnessSthrength);
+
+            // write normal to the color
+            //color = RGB(hitRec.m_normal.m_x, hitRec.m_normal.m_y, hitRec.m_normal.m_z);
         }
 
         return color;
