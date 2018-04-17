f0cee5aa44bcc5c5f1fc6191ad0b052c96132ff2
181847 <753090084@qq.com>
2018-04-17 22:55:10 +0800

    添加homogeneous vector（带4个分量的向量）。

---
+++ CommonClasses/hvector.cpp
@@ -0,0 +1,118 @@
+#include "hvector.h"
+
+namespace CommonClass
+{
+
+hvector::hvector(const Types::F32 x, const Types::F32 y, const Types::F32 z, const Types::F32 w)
+    :m_x(x), m_y(y), m_z(z), m_w(w)
+{
+    // empty
+}
+
+hvector::~hvector()
+{
+    // empty
+}
+
+hvector operator+(const hvector & v1, const hvector & v2)
+{
+    return hvector(v1.m_x + v2.m_x, v1.m_y + v2.m_y, v1.m_z + v2.m_z, v1.m_w + v2.m_w);
+}
+
+hvector operator-(const hvector & v1, const hvector & v2)
+{
+    return hvector(v1.m_x - v2.m_x, v1.m_y - v2.m_y, v1.m_z - v2.m_z, v1.m_w - v2.m_w);
+}
+
+hvector operator*(const hvector & v1, const hvector & v2)
+{
+    return hvector(v1.m_x * v2.m_x, v1.m_y * v2.m_y, v1.m_z * v2.m_z, v1.m_w * v2.m_w);
+}
+
+hvector operator/(const hvector & v1, const hvector & v2)
+{
+    return hvector(v1.m_x / v2.m_x, v1.m_y / v2.m_y, v1.m_z / v2.m_z, v1.m_w / v2.m_w);
+}
+
+hvector operator*(const Types::F32 s, const hvector & v)
+{
+    return hvector(s * v.m_x, s * v.m_y, s * v.m_z, s * v.m_w);
+}
+
+hvector operator*(const hvector & v, const Types::F32 s)
+{
+    return hvector(v.m_x * s, v.m_y * s, v.m_z * s, v.m_w * s);
+}
+
+hvector operator/(const hvector & v, const Types::F32 s)
+{
+    return hvector(v.m_x / s, v.m_y / s, v.m_z / s, v.m_w / s);
+}
+
+hvector & operator+=(hvector & v1, const hvector & v2)
+{
+    v1.m_x += v2.m_x;
+    v1.m_y += v2.m_y;
+    v1.m_z += v2.m_z;
+    v1.m_w += v2.m_w;
+    return v1;
+}
+
+hvector & operator-=(hvector & v1, const hvector & v2)
+{
+    v1.m_x -= v2.m_x;
+    v1.m_y -= v2.m_y;
+    v1.m_z -= v2.m_z;
+    v1.m_w -= v2.m_w;
+    return v1;
+}
+
+hvector & operator*=(hvector & v1, const hvector & v2)
+{
+    v1.m_x *= v2.m_x;
+    v1.m_y *= v2.m_y;
+    v1.m_z *= v2.m_z;
+    v1.m_w *= v2.m_w;
+    return v1;
+}
+
+hvector & operator/=(hvector & v1, const hvector & v2)
+{
+    v1.m_x /= v2.m_x;
+    v1.m_y /= v2.m_y;
+    v1.m_z /= v2.m_z;
+    v1.m_w /= v2.m_w;
+    return v1;
+}
+
+hvector & operator*=(hvector & v, Types::F32 s)
+{
+    v.m_x *= s;
+    v.m_y *= s;
+    v.m_z *= s;
+    v.m_w *= s;
+    return v;
+}
+
+hvector & operator/=(hvector & v, Types::F32 s)
+{
+    v.m_x /= s;
+    v.m_y /= s;
+    v.m_z /= s;
+    v.m_w /= s;
+    return v;
+}
+
+bool operator!=(const hvector & v1, const hvector & v2)
+{
+    for (unsigned int i = 0; i < 4; ++i)
+    {
+        if (v1.m_arr[i] != v2.m_arr[i])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+} // namespace CommonClass
---
+++ CommonClasses/hvector.h
@@ -0,0 +1,58 @@
+#pragma once
+#include "CommonTypes.h"
+
+namespace CommonClass
+{
+
+/*!
+    \brief homogeneous vector with four components
+*/
+class hvector
+{
+public:
+    union
+    {
+        struct
+        {
+            Types::F32 m_x;
+            Types::F32 m_y;
+            Types::F32 m_z;
+            Types::F32 m_w;
+        };
+        Types::F32 m_arr[4];
+    };
+public:
+    /*!
+        \brief construct a homogeneous vector, the w component default to be 1.0f, or the vector is default to be a LOCATION in the origin.
+    */
+    hvector(const Types::F32 x = 0.0f, const Types::F32 y = 0.0f, const Types::F32 z = 0.0f, const Types::F32 w = 1.0f);
+    ~hvector();
+};
+static_assert(sizeof(hvector) == 4 * sizeof(Types::F32), "sizeof hvector should be four F32");
+
+hvector  operator +  (const hvector&    v1,     const hvector&      v2      );
+hvector  operator -  (const hvector&    v1,     const hvector&      v2      );
+hvector  operator *  (const hvector&    v1,     const hvector&      v2      );
+hvector  operator /  (const hvector&    v1,     const hvector&      v2      );
+hvector  operator *  (const Types::F32  s,      const hvector&      v       );
+hvector  operator *  (const hvector&    v,      const Types::F32    s       );
+hvector  operator /  (const hvector&    v,      const Types::F32    s       );
+hvector& operator += (hvector&          v1,     const hvector&      v2      );
+hvector& operator -= (hvector&          v1,     const hvector&      v2      );
+hvector& operator *= (hvector&          v1,     const hvector&      v2      );
+hvector& operator /= (hvector&          v1,     const hvector&      v2      );
+hvector& operator *= (hvector&          v,      Types::F32          s       );
+hvector& operator /= (hvector&          v,      Types::F32          s       );
+bool     operator != (const hvector&    v1,     const hvector&      v2      );
+
+/*!
+    \brief prevent any other type do the comparison with hvector
+    In some situation, <hvector == float> will be transfered to <hvector == hvector(float, ...)>,
+    next two template delelte declare is to prevent the situation.
+*/
+template<typename T>
+bool operator != (const hvector& v, const T&) = delete;
+template<typename T>
+bool operator == (const hvector& v, const T&) = delete;
+
+} // namespace CommonClass
