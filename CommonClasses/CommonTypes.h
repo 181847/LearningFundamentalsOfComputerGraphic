ddf7be8c7cfac4a992296b7a437001e4878dc1ee
181847 <753090084@qq.com>
2018-04-18 22:51:39 +0800

    在CommonTypes当中添加一个圆周率的常量，并且添加一些注释。

--- CommonClasses/CommonTypes.h
+++ CommonClasses/CommonTypes.h
@@ -10,11 +10,30 @@ using U32 = unsigned int;
 using U8  = unsigned char;
 using I32 = int;
 
+/*!
+    \brief some constant for use.
+*/
 namespace Constant
 {
-	
+
+/*!
+    \brief the constant Pi in float
+*/
+const float  PI_F = 3.14159265358979f;
+
+/*!
+    \brief a positive float number close to zero.
+*/
 const Types::F32 EPSILON_F32 = 1e-20f;
+
+/*!
+    \brief positive max float number.
+*/
 const Types::F32 MAX_F32 = std::numeric_limits<Types::F32>::max();
+
+/*!
+    \brief negative min float number.
+*/
 const Types::F32 MIN_F32 = -MAX_F32; // std::numeric_limits<Types::F32>::min();
 
 
