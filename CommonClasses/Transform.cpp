4273389e9bc89e4b092b03700203564db3f4a989
181847 <753090084@qq.com>
2018-04-18 22:52:27 +0800

    添加变换矩阵类Transform，实现平移矩阵和轴旋转矩阵的构造方法。

---
+++ CommonClasses/Transform.cpp
@@ -0,0 +1,142 @@
+#include "Transform.h"
+
+
+namespace CommonClass
+{
+
+Transform::Transform()
+{
+    // NOTICE: vectors are column vector
+    m_column[0] = hvector(1.0f, 0.0f, 0.0f, 0.0f);
+    m_column[1] = hvector(0.0f, 1.0f, 0.0f, 0.0f);
+    m_column[2] = hvector(0.0f, 0.0f, 1.0f, 0.0f);
+    m_column[3] = hvector(0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+
+Transform::Transform(
+    const Types::F32 m11, const Types::F32 m12, const Types::F32 m13, const Types::F32 m14, 
+    const Types::F32 m21, const Types::F32 m22, const Types::F32 m23, const Types::F32 m24, 
+    const Types::F32 m31, const Types::F32 m32, const Types::F32 m33, const Types::F32 m34, 
+    const Types::F32 m41, const Types::F32 m42, const Types::F32 m43, const Types::F32 m44)
+{
+    // NOTICE: vectors are column vector
+
+    m_column[0] = hvector(
+        m11, 
+        m21,
+        m31,
+        m41);
+
+    m_column[1] = hvector(
+        m12,
+        m22,
+        m32,
+        m42);
+
+    m_column[2] = hvector(
+        m13,
+        m23,
+        m33,
+        m43);
+
+    m_column[3] = hvector(
+        m14,
+        m24,
+        m34,
+        m44);
+}
+
+Transform::~Transform()
+{
+}
+
+Transform Transform::Translation(const Types::F32 x, const Types::F32 y, const Types::F32 z)
+{
+    return Transform(
+        1.0f, 0.0f, 0.0f, x,
+        0.0f, 1.0f, 0.0f, y,
+        0.0f, 0.0f, 1.0f, z,
+        0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+Transform Transform::RotationX(const Types::F32 x)
+{
+    float 
+        cosTheta = std::cosf(x),
+        sinTheta = std::sinf(x);
+
+    return Transform(
+        1.0f, 0.0f,      0.0f,     0.0f,
+        0.0f, cosTheta, -sinTheta, 0.0f,
+        0.0f, sinTheta,  cosTheta, 0.0f,
+        0.0f, 0.0f,      0.0f,     1.0f);
+}
+
+Transform Transform::RotationY(const Types::F32 y)
+{
+    float
+        cosTheta = std::cosf(y),
+        sinTheta = std::sinf(y);
+
+    return Transform(
+        cosTheta,  0.0f, sinTheta, 0.0f,
+        0.0f,      1.0f, 0.0f,     0.0f,
+        -sinTheta, 0.0f, cosTheta, 0.0f,
+        0.0f,      0.0f, 0.0f,     1.0f);
+}
+
+Transform Transform::RotationZ(const Types::F32 z)
+{
+    float
+        cosTheta = std::cosf(z),
+        sinTheta = std::sinf(z);
+
+    return Transform(
+        cosTheta,  -sinTheta, 0.0f,     0.0f,
+        sinTheta,  cosTheta,  0.0f,     0.0f,
+        0.0f,      0.0f,      1.0f,     0.0f,
+        0.0f,      0.0f,      0.0f,     1.0f);
+}
+
+bool operator==(const Transform & m1, const Transform & m2)
+{
+    for (unsigned int i = 0; i < 4; ++i)
+    {
+        for (unsigned int j = 0; j < 4; ++j)
+        {
+            if (m1.m_column[i].m_arr[j] != m2.m_column[i].m_arr[j])
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool operator!=(const Transform & m1, const Transform & m2)
+{
+    for (unsigned int i = 0; i < 4; ++i)
+    {
+        for (unsigned int j = 0; j < 4; ++j)
+        {
+            if (m1.m_column[i].m_arr[j] != m2.m_column[i].m_arr[j])
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+hvector operator*(const Transform & m, const hvector & v)
+{
+    return hvector(
+        m.m_column[0].m_arr[0] * v.m_arr[0] + m.m_column[1].m_arr[0] * v.m_arr[1] + m.m_column[2].m_arr[0] * v.m_arr[2] + m.m_column[3].m_arr[0] * v.m_arr[3],
+        m.m_column[0].m_arr[1] * v.m_arr[0] + m.m_column[1].m_arr[1] * v.m_arr[1] + m.m_column[2].m_arr[1] * v.m_arr[2] + m.m_column[3].m_arr[1] * v.m_arr[3],
+        m.m_column[0].m_arr[2] * v.m_arr[0] + m.m_column[1].m_arr[2] * v.m_arr[1] + m.m_column[2].m_arr[2] * v.m_arr[2] + m.m_column[3].m_arr[2] * v.m_arr[3],
+        m.m_column[0].m_arr[3] * v.m_arr[0] + m.m_column[1].m_arr[3] * v.m_arr[1] + m.m_column[2].m_arr[3] * v.m_arr[2] + m.m_column[3].m_arr[3] * v.m_arr[3]
+    );
+}
+
+} // namespace CommonClass
---
+++ CommonClasses/Transform.h
@@ -0,0 +1,88 @@
+#pragma once
+#include "CommonTypes.h"
+#include "hvector.h"
+
+namespace CommonClass
+{
+
+/*!
+    \brief Transform is a 4x4 matrix support translation/rotation/viewTransformation...
+    we assume the vector are column vector, so the translation part is on the right of the matrix like
+    [
+    1, 0, 0, x
+    0, 1, 0, y
+    0, 0, 1, z
+    0, 0, 0, 1
+    ]
+*/
+class Transform
+{
+public:
+    /*!
+        \brief transform are defined by 4 column vectors.
+    */
+    hvector m_column[4];
+
+public:
+    /*!
+        \brief construct a unit matrix
+    */
+    Transform();
+
+    /*!
+        \brief construct a matrix by specifying all 16 components,
+        NOTICE: matix are consist of column vectors,
+        for example, if you want to make up a translation matrix(5, 4, 6),
+        you should set m14 = 5, m24 = 4, m34 = 6.
+    */
+    Transform(
+        const Types::F32 m11, const Types::F32 m12, const Types::F32 m13, const Types::F32 m14,
+        const Types::F32 m21, const Types::F32 m22, const Types::F32 m23, const Types::F32 m24,
+        const Types::F32 m31, const Types::F32 m32, const Types::F32 m33, const Types::F32 m34,
+        const Types::F32 m41, const Types::F32 m42, const Types::F32 m43, const Types::F32 m44);
+
+    ~Transform();
+
+    /*!
+        \brief construct a translation matrix for x/y/z.
+        \param x movement in x direction
+        \param y movement in y direction
+        \param z movement in z direction
+    */
+    static Transform Translation(const Types::F32 x = 0.0f, const Types::F32 y = 0.0f, const Types::F32 z = 0.0f);
+
+    /*!
+        \brief construct a rotation matrix around axis x
+        \param x rotate radians
+    */
+    static Transform RotationX(const Types::F32 x);
+    
+    /*!
+        \brief construct a rotation matrix around axis y
+        \param y rotate radians
+    */
+    static Transform RotationY(const Types::F32 y);
+    
+    /*!
+        \brief construct a rotation matrix around axis z
+        \param z rotate radians
+    */
+    static Transform RotationZ(const Types::F32 z);
+};
+
+/*!
+    \brief two transform matrix is same only if all coresponding component is same.
+*/
+bool operator == (const Transform& m1, const Transform& m2);
+
+/*!
+    \brief two transform matrix is same only if all coresponding component is same.
+*/
+bool operator != (const Transform& m1, const Transform& m2);
+
+/*!
+    \brief apply matrix transformation on the hvector, return an new instance.
+*/
+hvector operator * (const Transform& m, const hvector& v);
+
+} // namespace CommonClass
