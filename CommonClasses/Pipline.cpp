e9fb79787a3486b5fa7c7b83709acd17a80a4642
181847 <753090084@qq.com>
2018-04-20 13:27:56 +0800

    修改F32Buffer的缓冲大小单位，从float的数量改为基本的字节数量，便于比较。

--- CommonClasses/Pipline.cpp
+++ CommonClasses/Pipline.cpp
@@ -30,14 +30,14 @@ void Pipline::DrawLineList(const std::vector<unsigned int>& indices, const std::
         throw std::exception("indices of line list is not even");
     }
 
-    const unsigned int numVertices = lineEndPointList->GetSizeOfByte() / m_pso->m_vertexLayout.pixelShaderInputSize;
     const unsigned int vertexStride = m_pso->m_vertexLayout.pixelShaderInputSize;
+    const unsigned int numVertices = lineEndPointList->GetSizeOfByte() / vertexStride;
     unsigned char * pDataStart = lineEndPointList->GetBuffer();
 
     // for each two points, draw a segment
     for (unsigned int i = 0; i < numIndices - 1; i += 2)
     {
-        const ScreenSpaceVertexTemplate* pv1 = reinterpret_cast<const ScreenSpaceVertexTemplate *>(pDataStart + indices[i] * vertexStride);
+        const ScreenSpaceVertexTemplate* pv1 = reinterpret_cast<const ScreenSpaceVertexTemplate *>(pDataStart + indices[i    ] * vertexStride);
         const ScreenSpaceVertexTemplate* pv2 = reinterpret_cast<const ScreenSpaceVertexTemplate *>(pDataStart + indices[i + 1] * vertexStride);
 
         m_backBuffer->DrawBresenhamLine(
@@ -48,18 +48,19 @@ void Pipline::DrawLineList(const std::vector<unsigned int>& indices, const std::
     }
 }
 
-F32Buffer::F32Buffer(unsigned int size)
-    :m_numFloats(size)
+F32Buffer::F32Buffer(unsigned int sizeInByte)
+    :m_sizeInByte(sizeInByte)
 {
-    m_pBuffer = new Types::F32[m_numFloats];
+    assert(sizeInByte % 4 == 0 && "size of float is wrong, sizeInByte should be times of four");
+    m_pBuffer = new Types::F32[m_sizeInByte / 4];
 }
 
 F32Buffer::F32Buffer(F32Buffer && moveOtherBuffer)
 {
     m_pBuffer = moveOtherBuffer.m_pBuffer;
-    m_numFloats = moveOtherBuffer.m_numFloats;
+    m_sizeInByte = moveOtherBuffer.m_sizeInByte;
     moveOtherBuffer.m_pBuffer = nullptr;
-    moveOtherBuffer.m_numFloats = 0;
+    moveOtherBuffer.m_sizeInByte = 0;
 }
 
 F32Buffer::~F32Buffer()
@@ -77,12 +78,12 @@ unsigned char * F32Buffer::GetBuffer() const
 
 unsigned int F32Buffer::GetNumFloats() const
 {
-    return m_numFloats;
+    return m_sizeInByte / 4;
 }
 
 unsigned int F32Buffer::GetSizeOfByte() const
 {
-    return m_numFloats * sizeof(Types::F32);
+    return m_sizeInByte;
 }
 
 } // namespace CommonClass
--- CommonClasses/Pipline.h
+++ CommonClasses/Pipline.h
@@ -10,21 +10,25 @@ namespace CommonClass
 
     
 /*!
-    \brief the F32Buffer is used to create buffer of floats in stack, and free its memory when the var is out of life.
+    \brief the F32Buffer is used to create buffer of floats.
 */
 struct F32Buffer
 {
 private:
     Types::F32 * m_pBuffer = nullptr;
-    unsigned int m_numFloats = 0;
+    unsigned int m_sizeInByte = 0;
 
 public:
     /*!
         \brief construct the float buffer.
-        \param numFloats the number of the floats.
+        \param sizeInByte bytes size of the buffer
+        this buffer is consist of floats, but here we specific the size In BYTE,
+        so sizeInByte must be the times of 4 (sizeof(float32))
     */
-    F32Buffer(unsigned int numFloats);
+    F32Buffer(unsigned int sizeInByte);
     F32Buffer(F32Buffer && moveOtherBuffer);
+    F32Buffer(const F32Buffer&) = delete;
+    F32Buffer& operator = (const F32Buffer&) = delete;
     ~F32Buffer();
 
     /*!
@@ -117,17 +121,18 @@ inline void Pipline::DrawInstance(const std::vector<unsigned int>& indices, cons
 
     Transform& viewportTransformMat = m_pso->m_viewportTransform;
 
-    const unsigned int numFloats = vertices->GetNumFloats();
-    auto viewportTransData = std::make_unique<F32Buffer>(numFloats);
-
+    const unsigned int numBytes = vertices->GetSizeOfByte();
     const unsigned int vsInputStride = m_pso->m_vertexLayout.vertexShaderInputSize;
     const unsigned int psInputStride = m_pso->m_vertexLayout.pixelShaderInputSize;
 
+    // now the pipline is not complete, so for the simplification, we assume all the vertex is in the same size.
     assert(vsInputStride == psInputStride);
-    assert(numFloats % vsInputStride == 0 && "vertics data error, lack some data");
+    assert(numBytes % vsInputStride == 0 && "vertics data error, cannot ensure each vertex data is complete.");
 
     // compute the number of vertex.
-    const unsigned int numVertices = vertices->GetNumFloats() / vsInputStride;
+    const unsigned int numVertices = numBytes / vsInputStride;
+    // create shader input buffer.
+    auto viewportTransData = std::make_unique<F32Buffer>(numVertices * psInputStride);
 
     unsigned char * pSrcFloat  = vertices->GetBuffer();
     unsigned char * pDestFloat = viewportTransData->GetBuffer();
@@ -141,7 +146,7 @@ inline void Pipline::DrawInstance(const std::vector<unsigned int>& indices, cons
         pDestVertex->m_posH = viewportTransformMat * pSrcVertex->m_posH;
         
         // move to next data.
-        pSrcFloat += vsInputStride;
+        pSrcFloat  += vsInputStride;
         pDestFloat += psInputStride;
     }
 
