4294ab46c421c10d5f91b5250c4e99db2159239d
181847 <753090084@qq.com>
2018-04-24 09:50:24 +0800

    添加对函数参数的有效性判断。

--- CommonClasses/ScreenSpaceVertexTemplate.cpp
+++ CommonClasses/ScreenSpaceVertexTemplate.cpp
@@ -13,17 +13,22 @@ unsigned int ScreenSpaceVertexTemplate::NumRestFloat(const unsigned int realVert
 
 void Interpolate2(const ScreenSpaceVertexTemplate * pv1, const ScreenSpaceVertexTemplate * pv2, ScreenSpaceVertexTemplate * pResult, const Types::F32 u, const unsigned int realVertexSizeInBytes)
 {
+    assert(pv1      != nullptr);
+    assert(pv2      != nullptr);
+    assert(pResult  != nullptr);
+    assert(realVertexSizeInBytes >= sizeof(ScreenSpaceVertexTemplate::m_posH));
     const unsigned int numRestFloats = ScreenSpaceVertexTemplate::NumRestFloat(realVertexSizeInBytes);
 
     const Types::F32 v = 1.0f - u;
 
+    // interpolate screen space coordinate.
     pResult->m_posH = u * pv1->m_posH + v * pv2->m_posH;
 
+    // interpolate rest floats.
     for (unsigned int i = 0; i < numRestFloats; ++i)
     {
         pResult->m_restDates[i] = u * pv1->m_restDates[i] + v * pv2->m_restDates[i];
-    }
-
+    } // end for
 }
 
 } // namespace CommonClass
