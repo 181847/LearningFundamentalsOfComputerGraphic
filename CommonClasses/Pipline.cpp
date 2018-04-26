124c6e34a2084286b0fa09189777205b09dd0c83
181847 <753090084@qq.com>
2018-04-26 20:18:39 +0800

    将定义在Pipline文件里面的F32Buffer提取出来，单独用cpp/h文件定义。

--- CommonClasses/Pipline.cpp
+++ CommonClasses/Pipline.cpp
@@ -377,45 +377,5 @@ void Pipline::ClipLineList(
     }
 }
 
-F32Buffer::F32Buffer(unsigned int sizeInByte)
-    :m_sizeInByte(sizeInByte)
-{
-    assert(sizeInByte % 4 == 0 && "size of float is wrong, sizeInByte should be times of four");
-    m_pBuffer = new Types::F32[m_sizeInByte / 4];
-}
-
-F32Buffer::F32Buffer(F32Buffer && moveOtherBuffer)
-{
-    m_pBuffer = moveOtherBuffer.m_pBuffer;
-    m_sizeInByte = moveOtherBuffer.m_sizeInByte;
-    moveOtherBuffer.m_pBuffer = nullptr;
-    moveOtherBuffer.m_sizeInByte = 0;
-}
-
-F32Buffer::~F32Buffer()
-{
-    if (m_pBuffer != nullptr)
-    {
-        delete[] m_pBuffer;
-    }
-}
-
-unsigned char * F32Buffer::GetBuffer() const
-{
-    return reinterpret_cast<unsigned char*>(m_pBuffer);
-}
-
-unsigned int F32Buffer::GetNumFloats() const
-{
-    return m_sizeInByte / 4;
-}
-
-unsigned int F32Buffer::GetSizeOfByte() const
-{
-    return m_sizeInByte;
-}
-
-
-
 } // namespace CommonClass
 
--- CommonClasses/Pipline.h
+++ CommonClasses/Pipline.h
@@ -4,60 +4,11 @@
 #include "RasterizeImage.h"
 #include "PiplineStateObject.h"
 #include "ScreenSpaceVertexTemplate.h"
+#include "F32Buffer.h"
 
 namespace CommonClass
 {
 
-    
-/*!
-    \brief the F32Buffer is used to create buffer of floats.
-*/
-struct F32Buffer
-{
-private:
-    Types::F32 * m_pBuffer = nullptr;
-    unsigned int m_sizeInByte = 0;
-
-public:
-    /*!
-        \brief construct the float buffer.
-        \param sizeInByte bytes size of the buffer
-        this buffer is consist of floats, but here we specific the size In BYTE,
-        so sizeInByte must be the times of 4 (sizeof(float32))
-    */
-    F32Buffer(unsigned int sizeInByte);
-    F32Buffer(F32Buffer && moveOtherBuffer);
-    F32Buffer(const F32Buffer&) = delete;
-    F32Buffer& operator = (const F32Buffer&) = delete;
-    ~F32Buffer();
-
-    /*!
-        \brief get buffer pointer.
-    */
-    unsigned char * GetBuffer() const;
-
-    /*!
-        \brief return the size of the buffer in bytes.
-    */
-    unsigned int GetNumFloats() const;
-
-    /*!
-        \brief return the size of the buffer in Bytes.
-    */
-    unsigned int GetSizeOfByte() const;
-};
-
-/*!
-    \brief from a start address, get a vertex of specific index, and interpret it to the desired VERTEX_TYPE pointer.
-*/
-template<typename VERTEX_TYPE = unsigned char>
-inline VERTEX_TYPE * GetVertexPtrAt(unsigned char * pStartAddr, unsigned int vertexIndex, unsigned int vertexSizeInByte)
-{
-    return reinterpret_cast<VERTEX_TYPE * >(pStartAddr + vertexSizeInByte * vertexIndex);
-}
-
-
-    
 /*!
     \brief abstraction of graphic pipline
 */
