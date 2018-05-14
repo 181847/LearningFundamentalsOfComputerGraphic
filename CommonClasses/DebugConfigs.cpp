62e45b599225c6441b6ea052f4176182e34d5ee3
181847 <753090084@qq.com>
2018-05-14 20:52:35 +0800

    为主要的代码创建DebugClient对应的配置类。

---
+++ CommonClasses/DebugConfigs.cpp
@@ -0,0 +1,6 @@
+#include "DebugConfigs.h"
+
+/*!
+    \brief this file is mainly to set the Static memeber of the Debug Client Configuration Struct.
+*/
+bool DEBUG_CLIENT_CONF_LINE_CLIP_ERROR_ANALYSIS::Active = false;
---
+++ CommonClasses/DebugConfigs.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "DebugHelpers.h"
+
+/*!
+    \brief DebugConfigs.h is used to construct debug configurations for DebugClient
+    who is a template function and need a static member to indicate whether to break or not.
+*/
+
+/*!
+    \brief this debug client configuration is for error analysis in line clipping function.
+    should be used in "Pipline.cpp" about line clipping in homogenous clipping space
+*/
+class DEBUG_CLIENT_CONF_LINE_CLIP_ERROR_ANALYSIS
+{
+public:
+    /*!
+        \brief whether put break point in the run time.
+    */
+    static bool Active;
+
+    enum 
+    {
+        /*!
+           \brief this enum number is used to active the debug client
+           if this is set to NOT zero, it means disable it,
+           and all the places that use this debug client configuration will cause
+           an compile error.
+        */
+        ENABLE_CLIENT = 1
+    };
+};
