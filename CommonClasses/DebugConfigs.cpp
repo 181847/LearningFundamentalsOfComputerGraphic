0cf9b2883078e54071ae1f8eb811a93dae167131
181847 <753090084@qq.com>
2018-06-07 21:15:15 +0800

    为三角形绘制流程的调试添加相应的调试配置。

--- CommonClasses/DebugConfigs.cpp
+++ CommonClasses/DebugConfigs.cpp
@@ -4,3 +4,5 @@
     \brief this file is mainly to set the Static memeber of the Debug Client Configuration Struct.
 */
 bool DEBUG_CLIENT_CONF_LINE_CLIP_ERROR_ANALYSIS::Active = false;
+
+bool DEBUG_CLIENT_CONF_TRIANGL::Active = false;
--- CommonClasses/DebugConfigs.h
+++ CommonClasses/DebugConfigs.h
@@ -29,3 +29,26 @@ public:
         ENABLE_CLIENT = 1
     };
 };
+
+/*!
+    \brief the debug configurations of triangle rasterization.
+*/
+class DEBUG_CLIENT_CONF_TRIANGL
+{
+public:
+    /*!
+    \brief whether put break point in the run time.
+    */
+    static bool Active;
+
+    enum
+    {
+        /*!
+        \brief this enum number is used to active the debug client
+        if this is set to NOT zero, it means disable it,
+        and all the places that use this debug client configuration will cause
+        an compile error.
+        */
+        ENABLE_CLIENT = 1
+    };
+};
