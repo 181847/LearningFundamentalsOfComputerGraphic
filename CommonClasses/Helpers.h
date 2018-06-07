b09510436c366f8746a93bc57981a8ad8921305b
181847 <753090084@qq.com>
2018-06-07 21:13:18 +0800

    增加ShereRayTri用于生成放射状的三角形数据用于绘制。 并添加相应的工具宏。

--- CommonClasses/Helpers.h
+++ CommonClasses/Helpers.h
@@ -68,3 +68,49 @@ void SphereRay(
         rOut += segmentLength;
     } // end outer for loop
 }
+#define HELP_SPHERE_RAY_LAMBDA_PARAMETERS const Types::F32 x0, const Types::F32 y0, const Types::F32 x1, const Types::F32 y1, const unsigned int roundIndex, const unsigned int lineIndex
+
+void SphereRayTri(
+    std::function<void(
+        const Types::F32 x0, const Types::F32 y0,
+        const Types::F32 x1, const Types::F32 y1,
+        const Types::F32 x2, const Types::F32 y2,
+        const unsigned int roundIndex, const unsigned int triIndex)>
+    drawTri,
+    const Types::F32 centerLocationX,
+    const Types::F32 centerLocationY,
+    const Types::F32 segmentLength = 35.0f,
+    const Types::F32 startInnerRadius = 30.0f,
+    const Types::U32 numRounds = 5,
+    const Types::F32 radioOffset = 0.0f)
+{
+    Types::F32 deltaTheta   = 2.0f * Types::Constant::PI_F / 64.0f;
+    Types::F32 halfDT       = deltaTheta * 0.5f;
+    Types::F32 quadDT       = deltaTheta * 0.25f;
+    Types::F32 rIn          = startInnerRadius;
+    Types::F32 rOut         = rIn + segmentLength;
+
+    Types::F32 x0, y0, x1, y1, x2, y2;
+    for (unsigned int i = 0; i < numRounds; ++i)
+    {
+        for (unsigned int j = 0; j < 64; ++j)
+        {
+            Types::F32 offSetOfTheta = radioOffset + (i % 2) * halfDT;
+            Types::F32 theta1 = offSetOfTheta + deltaTheta * j;
+            Types::F32 theta2 = theta1 + deltaTheta;
+            Types::F32 theta3 = theta1 + halfDT;
+
+            x0 = centerLocationX + rIn  * std::cos(theta2);
+            y0 = centerLocationY + rIn  * std::sin(theta2);
+            x1 = centerLocationX + rIn  * std::cos(theta1);
+            y1 = centerLocationY + rIn  * std::sin(theta1);
+            x2 = centerLocationX + rOut * std::cos(theta3);
+            y2 = centerLocationY + rOut * std::sin(theta3);
+
+            drawTri(x0, y0, x1, y1, x2, y2, i, j);
+        }
+        rIn = rOut;
+        rOut += segmentLength;
+    } // end outer for loop
+}
+#define HELP_SPHERE_RAY_TRI_LAMBDA_PARAMETERS const Types::F32 x0, const Types::F32 y0, const Types::F32 x1, const Types::F32 y1, const Types::F32 x2, const Types::F32 y2, const unsigned int roundIndex, const unsigned int lineIndex
