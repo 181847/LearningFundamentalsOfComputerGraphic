6e1301055ede68638639199c4ab3ab874c2a895c
181847 <753090084@qq.com>
2018-06-07 21:14:01 +0800

    添加测试绘制TriangleList的测试单元。

--- TriangleRasterizeTest/TriangleRasterizeTest.cpp
+++ TriangleRasterizeTest/TriangleRasterizeTest.cpp
@@ -30,6 +30,7 @@
 #include "../CommonClasses/Pipline.h"
 #include "../CommonClasses/CoordinateFrame.h"
 #include "../CommonClasses/DebugConfigs.h"
+#include "../CommonClasses/Helpers.h"
 #pragma comment(lib, "CommonClasses.lib")
 
 using namespace CommonClass;
@@ -136,6 +137,148 @@ TEST_UNIT_START("directly draw triangle in screen space")
     std::string pictureIndex = "000";
     pipline.m_backBuffer->SaveTo("..\\RasterizeTest\\OutputTestImage\\PiplineTest\\TriangleTest\\screenSpaceTriangle_" + pictureIndex + ".png");
         
+TEST_UNIT_END;
+#pragma endregion
+
+
+#pragma region basic sphereRayTri test
+TEST_UNIT_START("basic sphereRayTri test")
+// temp struct for line drawing.
+        struct SimplePoint
+        {
+        public:
+            hvector m_position;
+            /*!
+                \brief used to store the sphere ray location information
+                x - round index
+                y - line index in the round
+                z = 0 means it's start point
+                z = 1 means it's end point
+            */
+            hvector m_rayIndex;
+            explicit SimplePoint(const hvector& pos)
+                :m_position(pos)
+            {
+                // empty
+            }
+        };
+
+        static_assert(sizeof(SimplePoint) == 2 * sizeof(hvector), "SimplePoint size is wrong");
+        // create and config pipline state object
+        auto pso = std::make_unique<PiplineStateObject>();
+        pso->m_primitiveType = PrimitiveType::TRIANGLE_LIST;
+        pso->m_vertexLayout.vertexShaderInputSize = sizeof(SimplePoint);
+        pso->m_vertexLayout.pixelShaderInputSize  = sizeof(SimplePoint);
+        
+        // the pixel shader will not work
+        // due to the imcompletation of the triangle pipeline.
+        pso->m_pixelShader = [](const ScreenSpaceVertexTemplate* pVertex)->RGBA {
+            const Types::F32   depth      = (pVertex->m_posH.m_z + 1.0f) * 0.5f;
+            const SimplePoint* pPoint     = reinterpret_cast<const SimplePoint*>(pVertex);
+            const Types::F32   red        = pPoint->m_rayIndex.m_z;
+
+            const Types::U32   roundIndex = static_cast<const Types::U32>(pPoint->m_rayIndex.m_x);
+            const Types::U32   lineIndex  = static_cast<const Types::U32>(pPoint->m_rayIndex.m_y);
+            //const Types::F32   isTheOne   = 22 < lineIndex && lineIndex < 34 ? 1.0f : 0.0f;
+            const Types::F32   isTheOne = roundIndex == 7 && 34 <= lineIndex && lineIndex <= 34  ? 1.0f : 0.0f;
+
+            RGBA               color     (isTheOne, 0.0f, 0.0f, 1.0f);
+            return color;
+        };
+
+        const Types::F32 LEFT(-1.0f), RIGHT(1.0f), BOTTOM(-1.0f), TOP(1.0f), NEAR(-0.5f), FAR(-4.0f);
+
+        // rotate the line a little.
+        Transform rotateY = Transform::RotationY(Types::Constant::PI_F / 3.0f);
+        Transform rotateZ = Transform::RotationZ(Types::Constant::PI_F / 3.0f);
+
+        Transform moveIntoScree = Transform::Translation(0.0f, 0.0f, -2.0f);
+
+        // perspective transformation
+        Transform perTrans = Transform::PerspectiveOG(LEFT, RIGHT, BOTTOM, TOP, NEAR, FAR);
+        
+        Transform pushInto = Transform::Translation(0.0f, 0.0f, -2.0f);
+
+        Transform mat = perTrans * pushInto * rotateZ * rotateY;
+
+        Transform toView = pushInto * rotateZ * rotateY;
+
+        Transform perspect = perTrans;
+
+        pso->m_vertexShader = [&mat, &toView, &perspect](const unsigned char * pSrcVertex, ScreenSpaceVertexTemplate * pDestV)->void {
+            const SimplePoint* pSrcH = reinterpret_cast<const SimplePoint*>(pSrcVertex);
+            SimplePoint* pDestH = reinterpret_cast<SimplePoint*>(pDestV);
+
+            //hvector inViewPos = toView * pSrcH->m_position;
+
+            //pDestH->m_position = perspect * inViewPos;
+            pDestH->m_position = pSrcH->m_position;
+            pDestH->m_rayIndex = pSrcH->m_rayIndex;
+        };
+
+        Viewport viewport;
+        viewport.left = 0;
+        viewport.right = UserConfig::COMMON_PIXEL_WIDTH - 1;
+        viewport.bottom = 0;
+        viewport.top = UserConfig::COMMON_PIXEL_HEIGHT - 1;
+        pso->SetViewport(viewport);
+
+        // create and set a pipline.
+        Pipline pipline;
+        pipline.SetPSO(std::move(pso));
+
+        // set a backbuffer
+        pipline.SetBackBuffer(std::make_unique<RasterizeImage>(
+            UserConfig::COMMON_PIXEL_WIDTH, 
+            UserConfig::COMMON_PIXEL_HEIGHT, 
+            RGBA::WHITE));
+
+        std::vector<SimplePoint> points;
+        std::vector<unsigned int> indices;
+        unsigned int numIndices = 0;
+
+        // create line segments in sphere ray.
+        SphereRayTri([&numIndices, &points, &indices](HELP_SPHERE_RAY_TRI_LAMBDA_PARAMETERS)->void {
+            
+            SimplePoint p0(hvector(x0, y0, 0.0f));
+            p0.m_rayIndex.m_x = roundIndex;
+            p0.m_rayIndex.m_y = lineIndex;
+            p0.m_rayIndex.m_z = 0;
+            points.push_back(p0);
+            indices.push_back(numIndices++);
+
+            SimplePoint p1(hvector(x1, y1, 0.0f));
+            p1.m_rayIndex.m_x = roundIndex;
+            p1.m_rayIndex.m_y = lineIndex;
+            p1.m_rayIndex.m_z = 1;
+            points.push_back(p1);
+            indices.push_back(numIndices++);
+
+            SimplePoint p2(hvector(x2, y2, 0.0f));
+            p2.m_rayIndex.m_x = roundIndex;
+            p2.m_rayIndex.m_y = lineIndex;
+            p2.m_rayIndex.m_z = 1;
+            points.push_back(p2);
+            indices.push_back(numIndices++);
+        }, 
+            0.0f, 0.0f,                     // center location
+            0.2f,                           // segment length
+            0.1f,                           // start radius
+            5                               // num rounds
+            ); // radio offset
+
+        auto vertexBuffer = std::make_unique<F32Buffer>(points.size() * sizeof(SimplePoint));
+        memcpy(vertexBuffer->GetBuffer(), points.data(), vertexBuffer->GetSizeOfByte());
+
+        {
+            TIME_GUARD;
+            //DebugGuard<DEBUG_CLIENT_CONF_TRIANGL> guard;
+            pipline.DrawInstance(indices, vertexBuffer.get());
+        }
+
+        std::string pictureIndex = "000";
+        pipline.m_backBuffer->SaveTo("..\\RasterizeTest\\OutputTestImage\\PiplineTest\\TriangleTest\\sphereTri_" + pictureIndex + ".png");
+
 TEST_UNIT_END;
 #pragma endregion
 
