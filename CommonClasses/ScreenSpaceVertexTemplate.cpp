8c4d3783253c027967dadc7264fc79e4ce9af7ea
181847 <753090084@qq.com>
2018-06-17 15:59:41 +0800

    添加三顶点插值的函数。

--- CommonClasses/ScreenSpaceVertexTemplate.cpp
+++ CommonClasses/ScreenSpaceVertexTemplate.cpp
@@ -36,4 +36,34 @@ void Interpolate2(const ScreenSpaceVertexTemplate * pv1, const ScreenSpaceVertex
     } // end for
 }
 
+void Interpolate3(
+    const ScreenSpaceVertexTemplate*    pv1,
+    const ScreenSpaceVertexTemplate*    pv2,
+    const ScreenSpaceVertexTemplate*    pv3,
+    ScreenSpaceVertexTemplate*          pResult,
+    const Types::F32                    alpha,
+    const Types::F32                    beta,
+    const Types::F32                    gamma,
+    const unsigned int                  realVertexSizeInBytes)
+{
+    
+    assert(pv1      != nullptr);
+    assert(pv2      != nullptr);
+    assert(pv3      != nullptr);
+    assert(pResult  != nullptr);
+    assert(realVertexSizeInBytes >= sizeof(ScreenSpaceVertexTemplate::m_posH));
+    const unsigned int numRestFloats = ScreenSpaceVertexTemplate::NumRestFloat(realVertexSizeInBytes);
+
+    for (int i = 0; i < 4; ++i)
+    {
+        pResult->m_posH.m_arr[i] = alpha * pv1->m_posH.m_arr[i] + beta * pv2->m_posH.m_arr[i] + gamma * pv3->m_posH.m_arr[i];
+    }
+
+    // interpolate rest floats.
+    for (unsigned int i = 0; i < numRestFloats; ++i)
+    {
+        pResult->m_restDates[i] = alpha * pv1->m_restDates[i] + beta * pv2->m_restDates[i] + gamma * pv3->m_restDates[i];
+    } // end for
+}
+
 } // namespace CommonClass
--- CommonClasses/ScreenSpaceVertexTemplate.h
+++ CommonClasses/ScreenSpaceVertexTemplate.h
@@ -33,10 +33,29 @@ static_assert(sizeof(ScreenSpaceVertexTemplate) == (4 + 1) * sizeof(Types::F32),
     \param realVertexSizeInBytes the real size of the vertex, all the vertex attributes are the float type.
 */
 void Interpolate2(
-    const ScreenSpaceVertexTemplate* pv1, 
-    const ScreenSpaceVertexTemplate* pv2, 
-    ScreenSpaceVertexTemplate* pResult, 
-    const Types::F32 t, 
-    const unsigned int realVertexSizeInBytes);
+    const ScreenSpaceVertexTemplate*    pv1, 
+    const ScreenSpaceVertexTemplate*    pv2, 
+    ScreenSpaceVertexTemplate*          pResult, 
+    const Types::F32                    t, 
+    const unsigned int                  realVertexSizeInBytes);
+
+/*!
+    \brief interpolate attributes bewteen three vertex
+    \param pv1~3 triangle vertex
+    \param result the interpolation result
+    \param alpha coefficient of pv1, if alpha == 1 then the result is aligned with pv1
+    \param beta  coefficient of pv2, if beta  == 1 then the result is aligned with pv2
+    \param gamma coefficient of pv3, if gamma == 1 then the result is aligned with pv3
+    \param realVertexSizeInBytes the real size of the vertex, all the vertex attributes are the float type.
+*/
+void Interpolate3(
+    const ScreenSpaceVertexTemplate*    pv1,
+    const ScreenSpaceVertexTemplate*    pv2,
+    const ScreenSpaceVertexTemplate*    pv3,
+    ScreenSpaceVertexTemplate*          pResult,
+    const Types::F32                    alpha,
+    const Types::F32                    beta,
+    const Types::F32                    gamma,
+    const unsigned int                  realVertexSizeInBytes);
 
 } // namespace CommonClass
