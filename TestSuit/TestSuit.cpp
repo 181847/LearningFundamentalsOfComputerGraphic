06eea8ff9a84c9f206b5186f8634f1f2fb5c18d5
刘洋 <753090084@qq.com>
2018-07-19 17:09:10 +0800

    添加测试用例。

--- TestSuit/TestSuit.cpp
+++ TestSuit/TestSuit.cpp
@@ -6,13 +6,39 @@
 
 class TestCase : public TestSuit::Case
 {
-public:
-	TestCase(){}
-    virtual ~TestCase() {}
-
     virtual void Run() override
     {
         printf("one method run.\n");
+    }
+};
+
+class TestCase2 : public TestSuit::Case
+{
+    virtual void Run() override
+    {
+        printf("second method run.\n");
+    }
+};
+
+class TestCase3 : public TestSuit::Case
+{
+    virtual void Run() override
+    {
+        printf("third method run.\n");
+    }
+};
+
+class MySuit : public TestSuit::Suit<TestCase, TestCase2, TestCase3>
+{
+public:
+    virtual void PrepareTotal() override
+    {
+        printf("MySuit called PrepareTotal\n");
+    }
+
+    virtual void PrepareBeforeEachCase() override
+    {
+        printf("MySuit called PrepareBeforeEachCase\n");
     }
 };
 
@@ -21,11 +47,8 @@ int main()
 {
     using namespace TestSuit;
 
-    Suit<TestCase> suit;
-    Suit<> suit2;
-
-    suit.Start();
-    suit2.Start();
+    MySuit mySuit;
+    mySuit.Start();
 
     getchar();
     return 0;
