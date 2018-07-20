536f70cf5732bd12c94b5dbe40e183db774de651
刘洋 <753090084@qq.com>
2018-07-20 18:49:11 +0800

    新建一个测试工程用Case和Suit管理测试代码。

---
+++ TestCasesInSuits/ReadMe.md
@@ -0,0 +1,5 @@
+# 项目描述
+这个项目是新的测试代码工程。
+使用新的**Suit**和**Case**来管理测试代码。
+
+详情参见**MyTools/TestTool/ReadMe.md**
---
+++ TestCasesInSuits/TestCasesInSuits.cpp
@@ -0,0 +1,11 @@
+// TestCasesInSuits.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
