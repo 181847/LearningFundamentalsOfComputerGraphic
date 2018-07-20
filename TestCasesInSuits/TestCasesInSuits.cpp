8c04f72041f70d404657140b7c4fba6cdb77f393
刘洋 <753090084@qq.com>
2018-07-20 19:33:28 +0800

    重新写了一个Suit，把一些常用的方法封装到CommonEnvironment中，方便代码重用。

--- TestCasesInSuits/TestCasesInSuits.cpp
+++ TestCasesInSuits/TestCasesInSuits.cpp
@@ -24,14 +24,100 @@
 
 namespace UserConfig
 {
-    const int COMMON_PIXEL_WIDTH = 512;
-    const int COMMON_PIXEL_HEIGHT = 512;
+    
 }
+
+
+
+using namespace CommonClass;
+
+/*!
+    \brief the common environment for pipline, for example: some useful pixel shader, pipline stateObject.
+*/
+class CommonEnvironment
+{
+public:
+    // temp struct for line drawing.
+    struct SimplePoint
+    {
+    public:
+        hvector m_position;
+        /*!
+        \brief used to store the sphere ray location information
+        x - round index
+        y - line index in the round
+        z = 0 means it's start point
+        z = 1 means it's end point
+        */
+        hvector m_rayIndex;
+        explicit SimplePoint(const hvector& pos)
+            :m_position(pos)
+        {
+            // empty
+        }
+    };
+
+public:
+    // membbers
+    int COMMON_PIXEL_WIDTH = 512;
+    int COMMON_PIXEL_HEIGHT = 512;
+
+public:
+    CommonEnvironment()
+    {
+        
+    }
+
+    std::unique_ptr<PiplineStateObject> GetCommonPSO()
+    {
+        auto pso = std::make_unique<PiplineStateObject>();
+        pso->m_primitiveType = PrimitiveType::LINE_LIST;
+        pso->m_vertexLayout.vertexShaderInputSize = sizeof(SimplePoint);
+        pso->m_vertexLayout.pixelShaderInputSize = sizeof(SimplePoint);
+        pso->SetViewport(GetCommonViewport());
+
+        return std::move(pso);
+    }
+
+    Viewport GetCommonViewport()
+    {
+        Viewport viewport;
+        viewport.left = 0;
+        viewport.right = COMMON_PIXEL_WIDTH - 1;
+        viewport.bottom = 0;
+        viewport.top = COMMON_PIXEL_HEIGHT - 1;
+        return viewport;
+    }
+
+    std::unique_ptr<Pipline> GetCommonPipline()
+    {
+        // create and set a pipline.
+        auto pipline = std::make_unique<Pipline>();
+        pipline->SetPSO(std::move(GetCommonPSO()));
+
+        // set a backbuffer
+        pipline->SetBackBuffer(std::make_unique<RasterizeImage>(
+            COMMON_PIXEL_WIDTH,
+            COMMON_PIXEL_HEIGHT,
+            RGBA::WHITE));
+
+        return std::move(pipline);
+    }
+};
+
 
 class TriangleTransformCase : public TestSuit::Case
 {
+private:
+    CommonEnvironment * pEnvironment;
+
 public:
-    TriangleTransformCase(): Case("triangle transform case") {}
+    TriangleTransformCase() : Case("triangle transform case") {}
+
+    virtual void SetEnvironment(void * pEnvironment) override
+    {
+        this->pEnvironment = reinterpret_cast<CommonEnvironment*>(pEnvironment);
+    }
 
     virtual void Run()
     {
@@ -39,32 +125,14 @@ public:
         // skip this test due to the bug of clipping line function.
         //return 0;
 
-        // temp struct for line drawing.
-        struct SimplePoint
-        {
-        public:
-            hvector m_position;
-            /*!
-            \brief used to store the sphere ray location information
-            x - round index
-            y - line index in the round
-            z = 0 means it's start point
-            z = 1 means it's end point
-            */
-            hvector m_rayIndex;
-            explicit SimplePoint(const hvector& pos)
-                :m_position(pos)
-            {
-                // empty
-            }
-        };
+        using SimplePoint = CommonEnvironment::SimplePoint;
+
 
         static_assert(sizeof(SimplePoint) == 2 * sizeof(hvector), "SimplePoint size is wrong");
-        // create and config pipline state object
-        auto pso = std::make_unique<PiplineStateObject>();
-        pso->m_primitiveType = PrimitiveType::LINE_LIST;
-        pso->m_vertexLayout.vertexShaderInputSize = sizeof(SimplePoint);
-        pso->m_vertexLayout.pixelShaderInputSize = sizeof(SimplePoint);
+
+        assert(pEnvironment);
+        auto pipline = pEnvironment->   GetCommonPipline();
+        auto pso     = pipline->        GetPSO();
 
         pso->m_pixelShader = [](const ScreenSpaceVertexTemplate* pVertex)->RGBA {
             const Types::F32   depth = (pVertex->m_posH.m_z + 1.0f) * 0.5f;
@@ -111,23 +179,6 @@ public:
             pDestH->m_rayIndex = pSrcH->m_rayIndex;
         };
 
-        Viewport viewport;
-        viewport.left = 0;
-        viewport.right = UserConfig::COMMON_PIXEL_WIDTH - 1;
-        viewport.bottom = 0;
-        viewport.top = UserConfig::COMMON_PIXEL_HEIGHT - 1;
-        pso->SetViewport(viewport);
-
-        // create and set a pipline.
-        Pipline pipline;
-        pipline.SetPSO(std::move(pso));
-
-        // set a backbuffer
-        pipline.SetBackBuffer(std::make_unique<RasterizeImage>(
-            UserConfig::COMMON_PIXEL_WIDTH,
-            UserConfig::COMMON_PIXEL_HEIGHT,
-            RGBA::WHITE));
-
         std::vector<SimplePoint> points;
         std::vector<unsigned int> indices;
         unsigned int numIndices = 0;
@@ -168,20 +219,36 @@ public:
         {
             COUNT_DETAIL_TIME;
             DebugGuard<DEBUG_CLIENT_CONF_LINE_CLIP_ERROR_ANALYSIS> guard;
-            pipline.DrawInstance(indices, vertexBuffer.get());
+            pipline->DrawInstance(indices, vertexBuffer.get());
         }
 
         std::wstring pictureIndex = L"final";
         std::wstring pictureName = L"lineClippingErrAnalysis_fixed_" + pictureIndex + L".png";
-        ImageWindow imgWnd(pipline.m_backBuffer.get(), pictureName);
+        ImageWindow imgWnd(pipline->m_backBuffer.get(), pictureName);
         imgWnd.BlockShow();
-        pipline.m_backBuffer->SaveTo(L"..\\OutputTestImage\\PiplineTest\\" + pictureName);
+        pipline->m_backBuffer->SaveTo(L"..\\OutputTestImage\\PiplineTest\\" + pictureName);
     }
+};
+
+class RasterizeSuit : public TestSuit::Suit<TriangleTransformCase>
+{
+public:
+    virtual void * PrepareBeforeEachCase(TestSuit::Case * pTheCase) override
+    {
+        CommonEnvironment * pNewEnv = new CommonEnvironment;
+        return pNewEnv;
+    }
+
+    virtual void FinishEachCase(TestSuit::Case * pTheCase, void * pEnvironment) override
+    {
+        delete reinterpret_cast<CommonEnvironment*>(pEnvironment);
+    }
+
 };
 
 int main()
 {
-    TestSuit::Suit<TriangleTransformCase> firstSuit;
+    RasterizeSuit firstSuit;
 
     firstSuit.Start();
 
