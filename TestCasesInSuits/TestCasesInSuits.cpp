9aa9f4caac41ba081c93a3c4cc60167f0a9d9d27
刘洋 <753090084@qq.com>
2018-07-30 22:24:29 +0800

    检查测试代码的执行效果。

--- TestCasesInSuits/TestCasesInSuits.cpp
+++ TestCasesInSuits/TestCasesInSuits.cpp
@@ -2,6 +2,7 @@
 #include "CommonHeaders.h"
 // include required suits here
 #include "TestBasicTypesSuit.h"
+#include "CaseAndSuitForCommonClasses.h"
 
 #pragma comment(lib, "MyTools/MyTool.lib")
 #pragma comment(lib, "CommonClasses.lib")
@@ -97,6 +98,9 @@ int main()
 
     BasicTypesTestSuit_Runable bttsRunable;
     bttsRunable.Start();
+
+    SuitForCommonClasses suitForCommonClasses;
+    suitForCommonClasses.Start();
 
     getchar();
     return 0;
