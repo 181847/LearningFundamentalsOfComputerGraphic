92c23ce5448c940ead43bf9a8f8c8d301b509629
刘洋 <753090084@qq.com>
2018-08-01 19:44:44 +0800

    将SuitQueueRunner转移到Suit定义的头文件中。

--- TestCasesInSuits/TestCasesInSuits.cpp
+++ TestCasesInSuits/TestCasesInSuits.cpp
@@ -10,47 +10,6 @@
 #pragma comment(lib, "MyTools/MyTool.lib")
 #pragma comment(lib, "CommonClasses.lib")
 
-/*!
-    \brief a helper class to Run multiple Suit at once.
-*/
-template<typename ... SUIT_TYPE_LIST>
-class SuitQueueRunner
-{
-protected:
-    std::vector<std::unique_ptr<TestSuit::SuitInterface>> m_suitQueue;
-
-public:
-    SuitQueueRunner()
-    {
-        std::unique_ptr<TestSuit::SuitInterface> list[] = { nullptr /* prevent array has size of ZERO */, std::make_unique<SUIT_TYPE_LIST>()... };
-        const int num = sizeof...(SUIT_TYPE_LIST);
-
-        for (int i = 1; i <= num; ++i)
-        {
-            m_suitQueue.push_back(std::move(list[i]));
-        }
-    }
-
-    /*!
-        \brief add on additional suit.
-        \param suit the suit you want to run.
-    */
-    void AddSuit(std::unique_ptr<TestSuit::SuitInterface> suit)
-    {
-        m_suitQueue.push_back(std::move(suit));
-    }
-
-    /*!
-        \brief fire up Suit::Start() for each suit instance.
-    */
-    void StartBatch()
-    {
-        for (auto& suit : m_suitQueue)
-        {
-            suit->Start();
-        }
-    }
-};
 
 int main()
 {
