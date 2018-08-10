93c295b251e03da42e54391e05fd973a38c77f9f
刘洋 <753090084@qq.com>
2018-08-10 22:16:39 +0800

    添加Filter类，实现简单的BoxFilter

---
+++ CommonClasses/Filter.cpp
@@ -0,0 +1,115 @@
+#include "Filter.h"
+#include "array"
+#include "DebugConfigs.h"
+
+namespace CommonClass
+{
+
+Filter::Filter(const unsigned int & step, const unsigned int & width, const unsigned int & height, const unsigned int & padding)
+    :m_step(step), m_width(width), m_height(height), m_padding(padding)
+{
+    // empty
+}
+
+Filter::~Filter()
+{
+    // empty
+}
+
+Image Filter::Convolve(const Image & img)
+{
+    const Loc IMG_S = { static_cast<int>(img.GetWidth()), static_cast<int>(img.GetHeight()) };
+    const Loc CON_S = ConvolutionSize(IMG_S, { static_cast<int>(m_width), static_cast<int>(m_height) });
+
+    Image convolutionResult(CON_S.X, CON_S.Y, RGBA::BLACK);
+
+    Loc start = { -static_cast<int>(m_padding), -static_cast<int>(m_padding) };
+
+    Loc rowHead = start;
+    for (unsigned int y = 0; y < CON_S.Y; ++y)
+    {
+        Loc horizontalScaner = rowHead;
+        for (unsigned int x = 0; x < CON_S.X; ++x)
+        {
+            convolutionResult.SetPixel(x, y, Step(img, horizontalScaner));
+            horizontalScaner = horizontalScaner.Go<Loc::RIGHT>(static_cast<int>(m_step));
+            if (x % 200 == 0)
+            {
+                printf("x = %4d, y = %4d\r", x, y);
+            }
+        }
+        rowHead = rowHead.Go<Loc::UP>(static_cast<int>(m_step));
+    }
+
+    return convolutionResult;
+}
+
+RGBA Filter::Step(const Image& img, const Loc& location)
+{
+    auto sampleLocs = GenerateSampleLocations(location);
+
+    std::array<float, 4> channels = {0.0f, 0.0f, 0.0f, 0.0f};
+
+    for (auto const& pos : sampleLocs)
+    {
+        auto sample = TryGetPixel(img, pos);
+
+        for (int i = 0; i < 4; ++i)
+        {
+            channels[i] += sample.m_arr[i];
+        }
+    }
+    
+    const int numSamples = sampleLocs.size();
+    for (auto& ch : channels)
+    {
+        ch /= numSamples;
+    }
+    
+    return RGBA(channels[0], channels[1], channels[2], channels[3]);
+}
+
+RGBA Filter::TryGetPixel(const Image& img, const Loc& location)
+{
+    const Loc IMG_S = { img.GetWidth(), img.GetHeight() };
+    if (!IMG_S.IsInclude(location)) // if out of boundary
+    {
+        return RGBA::BLACK;
+    }
+    else
+    {
+        return img.GetPixel(location.X, location.Y);
+    }
+}
+
+Filter::Loc Filter::ConvolutionSize(const Loc& imgSize) const
+{
+    Loc ret;
+    ret.X = std::floor((imgSize.X + 2 * m_padding -  m_width) * 1.0f / m_step + 1);
+    ret.Y = std::floor((imgSize.Y + 2 * m_padding - m_height) * 1.0f / m_step + 1);
+    return ret;
+}
+
+std::vector<CommonClass::Filter::Loc> Filter::GenerateSampleLocations(const Loc& bottomLeft)
+{
+    std::vector<Loc> retList;
+    
+    Loc step;
+    Loc RowHead = bottomLeft;
+    for (unsigned int y = 0; y < m_height; ++y)
+    {
+        Loc horizontalScaner = RowHead;
+
+        for (unsigned int x = 0; x < m_width; ++x)
+        {
+            retList.push_back(horizontalScaner);
+            horizontalScaner = horizontalScaner.Go<Loc::RIGHT>();
+        }
+
+        RowHead = RowHead.Go<Loc::UP>();
+    }
+
+    return retList;
+}
+
+}// namespace CommonClass
---
+++ CommonClasses/Filter.h
@@ -0,0 +1,111 @@
+#pragma once
+#include "Image.h"
+
+namespace CommonClass
+{
+
+/*!
+    \brief a Filter is used to convolve a image and return another image.
+*/
+class Filter
+{
+protected:
+    unsigned int m_step     = 1;
+    unsigned int m_width    = 3;
+    unsigned int m_height   = 3;
+    unsigned int m_padding  = 0;
+
+public:
+    /*!
+        \brief the help struct to store 2D location.
+    */
+    struct Loc
+    {
+        union
+        {
+            struct
+            {
+                int X;
+                int Y;
+            };
+            int ARR[2];
+        };
+
+        bool operator == (const Loc& other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+
+        /*!
+            \brief think this Loc as a boundary in {(0, 0), ..., (X - 1, Y - 1)},
+            return whether the point is in the set.
+        */
+        bool IsInclude(const Loc& point) const
+        {
+            return 
+                ! (point.X < 0
+                || point.X >= X
+                || point.Y < 0
+                || point.Y >= Y);
+        }
+
+        enum
+        {
+            UP = 0,
+            DOWN,
+            LEFT,
+            RIGHT
+        };
+        
+        template<int D>
+        Loc Go(int step = 1) const
+        {
+            return {
+                X + step * (       (D >> 1) * ((D & 1) * +2 - 1) ),
+                Y + step * ( (1 - (D >> 1)) * ((D & 1) * -2 + 1) )
+            };
+        }
+    };
+
+public:
+    Filter(
+        const unsigned int& step, 
+        const unsigned int& width, 
+        const unsigned int& height,
+        const unsigned int& padding);
+
+    ~Filter();
+
+    /*!
+        \brief convolve the image and return a new image.
+    */
+    Image Convolve(const Image& img);
+
+protected:
+    /*!
+        \brief for each convolve the pixel in location(x,y)
+        \param location the location is the virtual location in the img region,
+               it can extend the image size, take the location as the left bottom corner
+               of the filter and filter once of the img.
+    */
+    virtual RGBA Step(const Image& img, const Loc& location);
+
+    /*!
+        \brief try to get the pixel color of the img, if location is out of the boundary,
+        return some padding value, this function can be override by sub classes.
+    */
+    virtual RGBA TryGetPixel(const Image& img, const Loc& location);
+
+    /*!
+        \brief calculate the convolution result image size.
+    */
+    Loc ConvolutionSize(const Loc& imgSize) const;
+
+public:
+    /*!
+        \brief generate sample positions by the top left corner of one step of convolution.
+    */
+    std::vector<Loc> GenerateSampleLocations(const Loc& bottomLeft);
+};
+
+}// namespace CommonClass
